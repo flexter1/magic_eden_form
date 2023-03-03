bc23419e5d4fcd8e32bc8f174c2db2a656cd779c
flexter1 <37498580+flexter1@users.noreply.github.com>
2023-03-03 09:13:59 +0600

    Update main.py

--- main.py
+++ main.py
@@ -21,22 +21,25 @@ with open('sol.txt','r') as file:
     sol_list = [x.rstrip() for x in file.readlines()]
 
 def register(index):
-    mail = mails_list[index]
-    eth = eth_list[index]
-    sol = sol_list[index]
-
-    data = {
-        'entry.351426949': sol,
-        'entry.1658522070': eth,
-        'entry.1643820280':mail
-    }
-    resp = requests.post(urlResponse,data=data, headers={'Referer':urlReferer,'User-Agent': choice(ua_list)})
-    if resp.status_code==200:
-        with open('success.txt','a+') as file:
-            file.writelines(f"{mail}:{sol}:{eth}\n")
-            logger.success(f'{mail} | Success')
+    try:
+        mail = mails_list[index]
+        eth = eth_list[index]
+        sol = sol_list[index]
+    
+        data = {
+            'entry.351426949': sol,
+            'entry.1658522070': eth,
+            'entry.1643820280':mail
+        }
+        resp = requests.post(urlResponse,data=data, headers={'Referer':urlReferer,'User-Agent': choice(ua_list)})
+        if resp.status_code==200:
+            with open('success.txt','a+') as file:
+                file.writelines(f"{mail}:{sol}:{eth}\n")
+                logger.success(f'{mail} | Success')
+    except Exception as exc:
+        logger.error(exc)
 
 
 
 if __name__ == '__main__':
-    Pool(processes=5).map(register,range(len(mails_list)))
+    Pool(processes=3).map(register,range(len(mails_list)))
